45482fbaeeb6b5e735543a037e56cee226532b44
scm96steven <scm96steven@gmail.com>
2017-11-07 07:54:52 -0400

    Added product controller and index

---
+++ OpticSolutions.Repositories/Entitys/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpticSolutions.Repositories.Entitys
+{
+   public   class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ProductType { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int ReqWork { get; set; }
+
+    }
+}
