f719eb5f0798d45554f62f03d4d178a177b0408a
scm96steven <scm96steven@gmail.com>
2017-11-14 16:26:27 -0400

    Cambios del layout y creando controladores

---
+++ OpticSolutions/Controllers/ConsultsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpticSolutions.Controllers
+{
+    public class ConsultsController : Controller
+    {
+        // GET: Consults
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Consults/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Consults/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Consults/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Consults/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Consults/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Consults/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Consults/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
