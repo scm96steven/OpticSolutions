52c6e79eb43a6ad6f4da82cca998ffed8761b116
scm96steven <scm96steven@gmail.com>
2017-12-12 12:08:19 -0400

    Agregado metodo create appointment

--- OpticSolutions.Repositories/AppointmentRepository.cs
+++ OpticSolutions.Repositories/AppointmentRepository.cs
@@ -17,15 +17,23 @@ namespace OpticSolutions.Repositories
         {
             conn.ConnectionString = "Server=tcp:opticsolutions.database.windows.net,1433;Initial Catalog=OpticSolutions;Persist Security Info=False;User ID=osuser;Password=p@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             conn.Open();
-
-            var data = conn.Query<Appointment>("SELECT * FROM APPOINTMENTS").ToList();
     
         }
 
-        public void DatabaseTest()
+        public void CreateAppointment(Appointment ap)
         {
+            var queryParameters = new DynamicParameters();
+            queryParameters.Add("@names", ap.Names);
+            queryParameters.Add("@last_names", ap.LastNames);
+            queryParameters.Add("@email", ap.Email);
+            queryParameters.Add("@phone", ap.Phone);
+            queryParameters.Add("@cedula", ap.Cedula);
+            queryParameters.Add("@comment", ap.Comment);
+            queryParameters.Add("@date", DateTime.Now);
+            queryParameters.Add("@start_date", ap.StartDate);
+            queryParameters.Add("@end_date", ap.EndDate);
 
-
+            conn.Query("CREATE_APPOINTMENT", queryParameters, commandType: System.Data.CommandType.StoredProcedure);
         }
 
 
