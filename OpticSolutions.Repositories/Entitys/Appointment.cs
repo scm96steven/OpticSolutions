8f440d554f482156d25db271a90c2e41401961fa
scm96steven <scm96steven@gmail.com>
2017-12-19 12:12:43 -0400

    cambios crear cita

--- OpticSolutions.Repositories/Entitys/Appointment.cs
+++ OpticSolutions.Repositories/Entitys/Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,23 @@ namespace OpticSolutions.Repositories.Entitys
     {
         public int Id { get; set; }
         public string DoctorUsername { get; set; }
-        public int ClientId{ get; set; }
-        public string Phone { get; set; }
+        public string CellPhone { get; set; }
         public int Status { get; set; }
+
+        [Required(ErrorMessage = "El campo nombres es requerido")]
+        public string Names { get; set; }
+
+        [Required(ErrorMessage = "El campo apellidos es requerido")]
+        public string Last_Names { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo telefono es requerido")]
+        public string Phone { get; set; }
+
+
+        public string IdentificationCard { get; set; }
         public string Comment { get; set; }
         public DateTime Date { get; set; }
         public DateTime StartDate { get; set; }
