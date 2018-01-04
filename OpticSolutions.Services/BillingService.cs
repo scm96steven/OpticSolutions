f76b0e52bf6dd98583ef0ce23436b7b6e6889ccb
Frederick Ramirez Luciano <frederick5@hotmail.com>
2018-01-04 11:21:56 -0800

    Order Y Order Details

---
+++ OpticSolutions.Services/BillingService.cs
@@ -0,0 +1,28 @@
+﻿using OpticSolutions.Repositories;
+using OpticSolutions.Repositories.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpticSolutions.Services
+{
+    class BillingService
+    {
+        public BillingRepository repo = new BillingRepository();
+
+        public List<Orders> GetList(Orders Ord)
+        {
+            var data = GetList(Ord);
+            return data;
+        }
+
+        public void BillList(Orders Ord)
+        {
+            repo.BillList(Ord);
+        }
+
+
+    }
+}
