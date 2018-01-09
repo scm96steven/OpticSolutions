092cdbf41c33ffb86e93b0a2ab5ef2967dfd7cd1
Frederick Ramirez Luciano <frederick5@hotmail.com>
2018-01-09 12:48:43 -0800

    OrderList y OrderDetails

--- OpticSolutions/Controllers/BillingController.cs
+++ OpticSolutions/Controllers/BillingController.cs
@@ -8,82 +8,21 @@ namespace OpticSolutions.Controllers
 {
     public class BillingController : Controller
     {
-        // GET: Billing
-        public ActionResult Index()
+
+        public ActionResult OrderDetails()
         {
             return View();
         }
 
-        // GET: Billing/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
- 
         public ActionResult CreateOrder()
         {
             return View();
         }
 
-        // POST: Billing/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Billing/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult OrderList()
         {
             return View();
         }
 
-        // POST: Billing/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Billing/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Billing/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
